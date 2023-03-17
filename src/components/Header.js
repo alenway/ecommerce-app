5a50700ed9f9f199417c23d5bf1d715f26f1d82b
narendra <alenway97@gmail.com>
2023-03-17 15:01:36 +0530

    our store side menu
    completed the side menu option in our store page

--- src/components/Header.js
+++ src/components/Header.js
@@ -132,7 +132,7 @@ const Header = () => {
                     <NavLink className="text-white" to="/">
                       Home
                     </NavLink>
-                    <NavLink className="text-white" to="/">
+                    <NavLink className="text-white" to="/store">
                       Our Store
                     </NavLink>
                     <NavLink className="text-white" to="/">
